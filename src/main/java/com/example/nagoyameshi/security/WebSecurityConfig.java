0856be68db2fdb1b4f73b2b0a49f37ac29c80a68
shiro-461 <itouhi5656@gmail.com>
2026-02-04 09:51:07 +0900

    Add admin user management feature

--- src/main/java/com/example/nagoyameshi/security/WebSecurityConfig.java
+++ src/main/java/com/example/nagoyameshi/security/WebSecurityConfig.java
@@ -15,24 +15,26 @@ import org.springframework.security.web.SecurityFilterChain;
 public class WebSecurityConfig {
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-		http
-				.authorizeHttpRequests((requests) -> requests
-						.requestMatchers("/css/**", "/images/**", "/js/**", "/storage/**", "/", "/signup/**")
-						.permitAll() // すべてのユーザーにアクセスを許可するURL
-						.anyRequest().authenticated() // 上記以外のURLはログインが必要（どのロールでもOK）
-				)
-				.formLogin((form) -> form
-						.loginPage("/login") // ログインページのURL
-						.loginProcessingUrl("/login") // ログインフォームの送信先URL
-						.defaultSuccessUrl("/?loggedIn") // ログイン成功時のリダイレクト先URL
-						.failureUrl("/login?error") // ログイン失敗時のリダイレクト先URL
-						.permitAll())
-				.logout((logout) -> logout
-						.logoutSuccessUrl("/?loggedOut") // ログアウト時のリダイレクト先URL
-						.permitAll());
+        http
+            .authorizeHttpRequests((requests) -> requests
+                .requestMatchers("/css/**", "/images/**", "/js/**", "/storage/**", "/", "/signup/**").permitAll()  // すべてのユーザーにアクセスを許可するURL
+                .requestMatchers("/admin/**").hasRole("ADMIN")  // 管理者にのみアクセスを許可するURL
+                .anyRequest().authenticated()  // 上記以外のURLはログインが必要（どのロールでもOK）
+            )
+            .formLogin((form) -> form
+                .loginPage("/login")              // ログインページのURL
+                .loginProcessingUrl("/login")     // ログインフォームの送信先URL
+                .defaultSuccessUrl("/?loggedIn")  // ログイン成功時のリダイレクト先URL
+                .failureUrl("/login?error")       // ログイン失敗時のリダイレクト先URL
+                .permitAll()
+            )
+            .logout((logout) -> logout
+                .logoutSuccessUrl("/?loggedOut")  // ログアウト時のリダイレクト先URL
+                .permitAll()
+            );
 
-		return http.build();
-	}
+        return http.build();
+    }
 
 	@Bean
 	public PasswordEncoder passwordEncoder() {
